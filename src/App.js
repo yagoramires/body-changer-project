dc2cc74274a6d956f210a7dabaa5dee264f120c2
yagox <yago.ramiresx@gmail.com>
2022-10-11 09:38:47 -0300

    Configuração inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// Router
+import { Routes, Route } from 'react-router-dom';
+
+// Components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+// Pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
