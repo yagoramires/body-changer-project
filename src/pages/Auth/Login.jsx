4f66b800fb8800207551949f360b04736e639558
yagox <yago.ramiresx@gmail.com>
2022-10-13 18:40:37 -0300

    recuperação de senha

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
 
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { MdOutlineClose } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 import styles from './Auth.module.css';
@@ -13,11 +14,19 @@ const Login = () => {
   const [password, setPassword] = useState();
   // Error state
   const [error, setError] = useState();
+  // Message state
+  const [message, setMessage] = useState();
 
   // Modal state
-  const [active, setActive] = useState();
+  const [active, setActive] = useState(false);
 
-  const { login, error: loginError, loading } = useAuth();
+  const {
+    login,
+    resetPassword,
+    error: loginError,
+    message: loginMessage,
+    loading,
+  } = useAuth();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -47,19 +56,31 @@ const Login = () => {
       setError('Preencha o e-mail.');
       return;
     }
+
+    resetPassword(email);
   };
 
   useEffect(() => {
     if (loginError) {
       setError(loginError);
     }
-  }, [loginError]);
+    if (loginMessage) {
+      setMessage(loginMessage);
+    }
+  }, [loginError, loginMessage]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setError('');
-    }, 5000);
-  }, [error]);
+    if (error) {
+      setTimeout(() => {
+        setError('');
+      }, 5000);
+    }
+    if (message) {
+      setTimeout(() => {
+        setMessage('');
+      }, 5000);
+    }
+  }, [error, message]);
 
   return (
     <section className={styles.section}>
@@ -92,18 +113,38 @@ const Login = () => {
         <div className='divider'></div>
 
         <p>
-          Esqueceu sua senha? <Link>Recupere-a</Link>
+          Esqueceu sua senha?{' '}
+          <span onClick={() => setActive(true)}>Recupere-a</span>
         </p>
         <p>
           Não possui uma conta? <Link to='/register'>Registre-se</Link>
         </p>
       </form>
 
-      <div className='modal'>
-        <form
-          onSubmit={handleResetPass}
-          className={`form ${active ? 'show' : ''}`}
-        ></form>
+      <div className={`modal ${active ? '' : 'hide'}`}>
+        <form onSubmit={handleResetPass} className='form'>
+          <div className='modal-close-container'>
+            <p>Recuperação de Senha</p>
+            <MdOutlineClose onClick={() => setActive(false)} />
+          </div>
+          <input
+            type='email'
+            value={email || ''}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder='E-mail'
+          />
+
+          {loading ? (
+            <AiOutlineLoading3Quarters className='loading' />
+          ) : (
+            <input type='submit' value='Enviar' />
+          )}
+
+          {message && <p className='success'>{message}</p>}
+          {error && <p className='error'>{error}</p>}
+        </form>
       </div>
     </section>
   );
